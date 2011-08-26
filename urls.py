0637df570c76f0da1724ed0f04e526afdc3cfb69
Kaitlin <katycorp@gmail.com>
2011-08-26 00:05:11 -0500

    ito serve media correctly in dev server  Please enter the commit message for your changes. Lines starting

--- urls.py
+++ urls.py
@@ -17,3 +17,11 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^assets/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    )
